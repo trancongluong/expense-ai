f819726d2e43a922f811635ee124c237d5c6a0e8
trancongluong <33579011+trancongluong@users.noreply.github.com>
2026-07-13 08:26:00 +0700

    Add javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const saveButton = document.getElementById("saveButton");
+
+    saveButton.addEventListener("click", () => {
+
+        alert("Expense AI hoạt động!");
+
+    });
+
+});
